c8e448d6607f544a77701f95a889db7ca668edfc
Stepan <tstepa037@gmail.com>
2022-12-20 19:16:52 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число 1: ");
+int a = int.Parse(Console.ReadLine()!);
+
+if (a%2 == 0)
+{
+    Console.WriteLine("Ответ да");
+}
+else
+{
+    Console.WriteLine("Ответ нет");
+}
