21821b607d96506a2f26f4217131d87e9d9a6aa8
Stepan <tstepa037@gmail.com>
2022-12-20 18:36:42 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число 1: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число 2: ");
+int b = int.Parse(Console.ReadLine()!);
+
+
+if (a>b)
+{
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine(b);
+}
